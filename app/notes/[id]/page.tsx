d3a1b54576265fecfd42e9748c1a186753b5767b
vladkapa <tembioz@proton.me>
2025-09-09 04:19:15 +0300

    dep1

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -5,6 +5,7 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 import NoteDetailsClient from "./NoteDetails.client";
+import { Metadata } from "next";
 
 interface NoteDetailProps {
   params: Promise<{ id: string }>;
